c8d578a160cd3fb7ee141a7f91ae133332acdf1e
Colin Cotter <colin.cotter@imperial.ac.uk>
2019-03-20 10:54:06 +0000

    the actual mean values of the three trajectories

--- floats/read_lagrangian.py
+++ floats/read_lagrangian.py
@@ -3,5 +3,15 @@ with open('lagrangian.json') as infile:
     d = json.load(infile)
 
 import json
-with open('lagrangian_extra.json') as infile:
+with open('lagrangian_extra_with_means.json') as infile:
     d = json.load(infile)
+
+import numpy
+latmean = numpy.zeros(3)
+lonmean = numpy.zeros(3)
+for i in range(3):
+    latmean[i] = numpy.mean(d[i]['lat'])
+    lonmean[i] = numpy.mean(d[i]['lon'])
+
+latmean.tofile('latmean.txt', sep=' ')
+lonmean.tofile('lonmean.txt', sep=' ')

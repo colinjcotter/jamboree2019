07daab4f02977107e0c1e6e28633d4f56ac6992f
Colin Cotter <colin.cotter@imperial.ac.uk>
2019-03-20 11:00:36 +0000

    computed lagrangian error group1

--- floats/group1/read_outputfile.py
+++ floats/group1/read_outputfile.py
@@ -12,13 +12,18 @@ pred_lat = output[1,:]
 err_lon = output[2,:]
 err_lat = output[3,:]
 
+latmean = np.fromfile('../latmean.txt', sep=' ')
+lonmean = np.fromfile('../lonmean.txt', sep=' ')
 
-print("Predicted Longitudes")
-print(pred_lon)
-print("1 Sigma on Longitudes")
-print(err_lon)
-print("Predicted Latitudes")
-print(pred_lat)
-print("1 Sigma on Latitudes")
-print(err_lat)
+print(pred_lon.shape)
+print(pred_lat.shape)
+print(err_lon.shape)
+print(err_lat.shape)
+print(latmean.shape)
+print(lonmean.shape)
+
+prediction_error = np.sqrt(np.sum((pred_lon-lonmean)**2/err_lon + (pred_lat-latmean)**2/err_lat))
+
+prediction_error = np.array([prediction_error])
+prediction_error.tofile('error.txt',sep=' ')
 
